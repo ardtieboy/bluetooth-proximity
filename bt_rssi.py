796e24e2768d656ad139fe838292ff1473f7357b
Ard Scheirlynck <ard_scheirlynck@msn.com>
2020-03-09 21:08:53 +0100

    push

--- bt_rssi.py
+++ bt_rssi.py
@@ -24,7 +24,7 @@ class BluetoothRSSI(object):
         print(bt.ACL_LINK)
         print(type(bt.ACL_LINK))
         reqstr = struct.pack("6sB17s", bt.str2ba(self.addr), bt.ACL_LINK, bytes("\0" * 17, 'utf-8'))
-        request = array.array("c", reqstr)
+        request = array.array("b", reqstr)
         handle = fcntl.ioctl(self.hci_fd, bt.HCIGETCONNINFO, request, 1)
         handle = struct.unpack("8xH14x", request.tostring())[0]
         self.cmd_pkt = struct.pack('H', handle)
